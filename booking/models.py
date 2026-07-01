62b4bbf6833a9eb006394d84d351cce247f97c53
Denis Mesmer <denis22812281@gmail.com>
2026-07-01 06:38:46 +0300

    Fix: remove ImageField, use railway.toml startCommand

--- booking/models.py
+++ booking/models.py
@@ -19,7 +19,7 @@ class Service(models.Model):
 class Master(models.Model):
     name = models.CharField(max_length=100)
     specialization = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='masters/', blank=True)
+    photo = models.CharField(max_length=200, blank=True)
     bio = models.TextField(blank=True)
 
     def __str__(self):
